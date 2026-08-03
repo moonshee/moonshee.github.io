eb3fad3146f482f9ec36144d17eededa6e74a6ea
Dr. Fujihara <altes433@gmail.com>
2026-08-03 07:00:45 -0700

    New COBOL file
    Real estate news from Exec Director regarding Phelan Hills plans.

--- dongon/carcinoma.cob
+++ dongon/carcinoma.cob
@@ -1,7 +1,29 @@
-<div>
+<div style="background:black">
 <font color="lightgreen">
-000100 CARCINOMA DIVISION.<br>
-000200 PROGRAM-ID. COBOL AI.
-<div>
+
+*-------------------------------------------------
+<br>*  CARCINOMA DIVISION – Identification header
+<br>*-------------------------------------------------
+<br>000100  IDENTIFICATION DIVISION.
+<br>000200  PROGRAM-ID. COBOL-AI.
+<br>*-------------------------------------------------
+<br>*  ENVIRONMENT DIVISION – (essential for EL MUNDO)
+<br>*-------------------------------------------------
+<br>000300  ENVIRONMENT DIVISION.
+<br>000400  CONFIGURATION SECTION.
+<br>*-------------------------------------------------
+<br>*  DATA DIVISION – CRC data identified. Downloading...
+<br>*-------------------------------------------------
+<br>000500  DATA DIVISION.
+<br>*-------------------------------------------------
+<br>*  PROCEDURE DIVISION – Program logic block ENGAGED
+<br>*-------------------------------------------------
+<br>000600  PROCEDURE DIVISION.
+<br>000700  MAIN-SECTION.
+<br>*--- Display a short message from the Executive Director ---*
+<br>000800      DISPLAY "Message from Dr. Fujihara: "
+<br>000810      DISPLAY "«Our firm remains committed to curing cancer and Rubus Xtrakkt (TM) continues as our ichiban.  We tested Punica extract and found it to be quite strong indeed.  However, we cannot say that PE (punica extract) is safe to use unlike Rubus Maximus, the flagship strain developed by our team in Tokyo which will soon move to Phelan Hills, Californie pending several fully-funded Nihonjin-led real estate acquisitions in the Phelan Hills and Pinon Hills area scheduled for 2026-2027.»".
+<br>000820      STOP RUN.
+</div>
 
 
